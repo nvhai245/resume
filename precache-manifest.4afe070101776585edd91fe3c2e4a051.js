ef806268a0f3eeeb0efb99a64351905c76ccde99
Hari N <nvhai245@gmail.com>
2020-02-09 00:51:11 +0700

    Updates

---
+++ precache-manifest.4afe070101776585edd91fe3c2e4a051.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c666957d8428b6ab431458508a5c2a62",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "8cbfb9790f2aa4bad3aa",
+    "url": "/resume/static/css/2.0eaf8e31.chunk.css"
+  },
+  {
+    "revision": "7beb4238ec1c7c50258a",
+    "url": "/resume/static/css/main.54f57a6a.chunk.css"
+  },
+  {
+    "revision": "8cbfb9790f2aa4bad3aa",
+    "url": "/resume/static/js/2.922f069d.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/resume/static/js/2.922f069d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7beb4238ec1c7c50258a",
+    "url": "/resume/static/js/main.7f714750.chunk.js"
+  },
+  {
+    "revision": "540de6763af4833a1da8",
+    "url": "/resume/static/js/runtime-main.64f02ed5.js"
+  }
+]);
